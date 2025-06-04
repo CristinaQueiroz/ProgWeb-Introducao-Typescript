b54b93808128ccb5f2180476c86cd0c15118d5b0
Maria Cristina de Souza Queiroz <2023105070032@aluno.ifro.edu.br>
2025-06-04 17:03:10 -0400

    Introdução ao Typerscript - parte 2

---
+++ Estrutura de controle.ts
@@ -0,0 +1,16 @@
+// === ESTRUTURAS DE CONTROLE ===
+
+// IF - Se
+// ELSE IF -Senão se
+// ELSE - Sen]ao
+
+let idade: number = 28;
+
+if (idade >= 18){
+    //ação
+    console.log('Maior de idade');
+}else if(idade >=16){
+    console.log('Pode Votar');
+} else{
+    console.log('Menor de idade');
+}
